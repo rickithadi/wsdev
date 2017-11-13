4e1137e8eafa8d3022475d80578549cf7e9b11a3
rickithadi <rickithadi@gmail.com>
2017-11-13 20:15:27 +0800

    first

---
+++ crispy/Models/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace crispy.Models
+{
+    public class Movie
+    {
+        public int Id{get; set;}
+        public String Name { get; set; }
+       
+
+    }
+
+    
+}
